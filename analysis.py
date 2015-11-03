f0bf756fac4d5b9df01f6ff6d3318792ee8f54b8
Kevin Brown <kevin.s.brown@uconn.edu>
2015-11-02 22:08:45 -0500

    Added phi(t) and spike flagging routines

--- analysis.py
+++ analysis.py
@@ -7,12 +7,80 @@ analysis.py
 
 Created by Kevin Brown on 2015-03-17.
 """
-from numpy import log,histogram,nonzero,delete
+from numpy import log,histogram,nonzero,delete,zeros_like,array,exp,mean,abs
+
+def phi_of_t(y,group=None):
+    '''
+    For y(i,t), computes:
+
+        phi(t) = |<exp(i*y(t))>|
+
+    where the average is computed only over the rows indicated in group.  If
+    group is None, the average is computed over the entire array.
+
+    INPUT:
+        y: array-like, required
+            y should be an N x t array of floats/doubles.
+
+        group : list, optional
+            group should be a list of rows of y to include in the calculation.
+            if group is None, all rows are used.
+
+    OUTPUT:
+        phi : array
+            phi will be an array of size 1 x t, representing the synchonization
+            index, as a function of time, computed over the input row group
+    '''
+    if group is None:
+        group = range(0,y.shape[0])
+    # set up the array exp(i*y)
+    eiy = exp(1j*y)
+    # do the averaging
+    phi = eiy[group,:].mean(axis=0)
+    # now the modulus
+    return abs(phi)
+
+
+def convert_to_spikes(y,sorting='upper',thresh=1.0):
+    '''
+    Accepts an input array y (N x t), representing amplitude or something
+    similar, and returns an array of 1's and 0's of the same size giving
+    spike locations, defined as times in which the amplitude of a node
+    was above or below a user-defined threshold.
+
+    INPUT:
+        y: array-like, required
+           N x t array; time series for each of the N nodes in the model
+
+        sorting: string, optional
+            set to 'upper' to flag times where y(i,t) > thresh, and
+            'lower' to flag times where y(i,t) < thresh
+
+        thresh: float, optional
+            cutoff for flagging times; spikes can probably be identified as
+            times when y(i,t) > thresh or y(i,t) < thresh.  For example, with
+            sorting = 'upper', thresh might be 1.0 (depending on the EOM for
+            a single node).  If sorting = 'lower', thresh might be 1.0e-06
+            (if we are looking for times when the node has reset to a value
+            of zero).
+
+    OUTPUT:
+        s : array-like
+            N x t array of 1's and 0's, with 'interesting' times in y having
+            the value 1.0
+    '''
+    s = zeros_like(y)
+    if sorting is 'upper':
+        s[y >= thresh] = 1
+    else:
+        s[y <= thresh] = 1
+    return s
+
 
 def entropy(x,bins=10,est='ML'):
-    """
+    '''
     Computes the entropy of a continuous (unbinned) set of data x.
-    
+
     INPUT:
         x: array-like, required
            data for which to compute H[x]
@@ -29,7 +97,7 @@ def entropy(x,bins=10,est='ML'):
 
     OUTPUT:
         H[x] : entropy of x, measured in nats
-    """
+    '''
     cx = histogram(x,bins)[0]
     pofx = (1.0*cx)/cx.sum()
     # remove zero bins to avoid numerical problems
