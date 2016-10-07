f78205dec2d80542ca2f451e9821aa19dc6c1ccc
Kevin Brown <kevin.s.brown@uconn.edu>
2016-10-07 14:55:38 -0400

    Improved raster plot to do bars or dots.

--- plotting.py
+++ plotting.py
@@ -9,46 +9,35 @@ This module has wrappers for making figures that we find we often need.
 
 import pylab
 import networkx as nx
-from numpy import ceil,exp,array
+from numpy import ceil,exp,array,where,arange,hstack
 
-def plot_spike_plot(spike_raster,ptsize=10):
+
+def plot_spike_raster(spike_array,figtype='image',msize='9'):
     '''
     Accepts an input integer array of 1's and 0's (a 1 denoting a spike fired
-    in that time bin) and plots the spikes as black dots.  Adjust the size of
-    the points (in points) using ptsize.
+    in that time bin) and uses either:
+        (1) figtype:'image' : imshow, with reasonable options to produce a b/w image.
+            Spikes are shown in black.
+        (2) figtype:'dots' : plot, in which each spike is represented as a '.'  size
+            of the dot is controlled via msize.
     '''
     fig = pylab.figure()
-    # time and node numbers
-    N,T = spike_raster.shape
-    # linear array of nodes and times where spikes are present
-    nodes = array(range(N))
-    times = array(range(T))
-    x = []
-    y = []
-    for i in xrange(0,N):
-        spikeloc = times[spike_raster[i,:] == 1]
-        x.append(spikeloc)
-        y.append(np.repeat(nodes[i],len(spikeloc)))
-    x = np.hstack(x)
-    y = np.hstack(y)
-    pylab.scatter(x = x, y = y, s = ptsize)
+    if figtype is 'image':
+        # the 1-spike_raster converts zeros (no spikes) to white
+        pylab.imshow(1-spike_array,interpolation='none',aspect='auto',cmap='gray',figure=fig)
+    else:
+        # spikes are present where there is a '1' in the spike_array
+        N = spike_array.shape[0]
+        t = arange(spike_array.shape[1])
+        for i in range(N):
+            spikeloc = where(spike_array[i,:] == 1)[0]
+            pylab.plot(t[spikeloc],i*spike_array[i,spikeloc],'k.',markersize=msize)
+            pylab.xlim([-1,len(t)+1])
+            pylab.ylim([-0.5,N-0.5])
     fig.axes[0].set_xticks([])
     fig.axes[0].set_yticks([])
     return fig
 
-
-def plot_spike_raster(spike_raster):
-    '''
-    Accepts an input integer array of 1's and 0's (a 1 denoting a spike fired
-    in that time bin) and uses imshow, with reasonable options to produce a
-    b/w image.  Spikes are shown in black.
-    '''
-    fig = pylab.figure()
-    # the 1-spike_raster converts zeros (no spikes) to white
-    pylab.imshow(1-spike_raster,interpolation='none',aspect='auto',cmap='gray',figure=fig)
-    fig.axes[0].set_xticks([])
-    fig.axes[0].set_yticks([])
-    return fig
 
 def plot_network_ring(G,defcolor='k',ncData=None,layout='radial'):
     '''
