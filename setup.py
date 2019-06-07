ac186e4d99354fcbba336f1a824b1496cebb0559
Kyra Kadhim <kyrakadhim@gmail.com>
2019-06-06 22:15:10 -0700

    Changed Version
    Changed from 1.7.0 to 1.8.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class PyTest(Command):
         raise SystemExit(errno)
 
 setup(name='pydynet',
-      version='1.7.0',
+      version='1.8.0',
       description='Python Package for Dynamics on Networks',
       author='Kevin Brown and Ann Hermundstad',
       author_email='kevin.s.brown@uconn.edu',
